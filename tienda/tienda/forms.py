540533409ce0aa60b1d222c966c0c74de6ad3eb5
claudio.caniullan <claudio.caniullan@usach.cl>
2021-02-08 20:41:36 -0300

    registro en proceso de validacion de usuarios existentes

--- tienda/tienda/forms.py
+++ tienda/tienda/forms.py
@@ -1,11 +1,46 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
 	username = forms.CharField(
 		required=True, 
 		min_length=4, 
 		max_length=50,
+		# aplicamos estilo a nuestro input con widget
+		widget=forms.TextInput(attrs={
+			'class':'form-control',
+			'id':'username',
+			})
 		)
 	
-	email = forms.EmailField(required=True)
-	password = forms.CharField(required=True)
+	email = forms.EmailField(
+		required=True,
+		widget=forms.EmailInput(attrs={
+			'class':'form-control',
+			'id':'email',
+			'placeholder':'tienda@tienda.com'
+			})
+		)
+		
+	password = forms.CharField(
+		required=True,
+		widget=forms.PasswordInput(attrs={
+			'class':'form-control',
+			'id':'password',
+			})
+			)
+
+	def claean_username(self):
+		#obtenemos los inputs del formulario
+		username = self.cleaned_data.get('username')
+		#verificamos si existen en db
+		if User.objects.filter(username=username).exists():
+			raise forms.ValidationError('El username ya se encuentra en uso')
+		# de no existir
+		return username
+
+
+
+
+
+
--- tienda/tienda/views.py
+++ tienda/tienda/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from django.contrib.auth.models import User
 
 from .forms import RegisterForm
 
@@ -44,5 +45,21 @@ def logout_usuario(request):
 
 
 def registro(request):
-	form = RegisterForm()
-	return render(request, 'users/registro.html', {'form':form})
+	#generamos un formulario con datos del cliente o vacio
+	form = RegisterForm(request.POST or None)
+    
+    # vamos a validar los datos del formulario
+	if request.method == 'POST' and form.is_valid():
+		username = form.cleaned_data.get('username')
+		email = form.cleaned_data.get('email')
+		password = form.cleaned_data.get('password')
+        
+        # creamos la instacia con los datos ingresados a formulario
+		user = User.objects.create_user(username, email, password)
+		if user:
+			# al crear el usuario se logea y redirige
+			login(request, user)
+			messages.success(request, 'Usuario creado exitosamente')
+			return redirect('index')
+
+	return render(request, 'users/registro.html', {'form':form}) 
