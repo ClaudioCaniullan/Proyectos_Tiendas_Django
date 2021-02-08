a7732dd5fd7c5d3f006b056af007cf4911a2125e
claudio.caniullan <claudio.caniullan@usach.cl>
2021-02-08 20:23:19 -0300

    template de registro ahora se visualiza, siguen mejoras

--- tienda/tienda/forms.py
+++ tienda/tienda/forms.py
@@ -4,7 +4,7 @@ class RegisterForm(forms.Form):
 	username = forms.CharField(
 		required=True, 
 		min_length=4, 
-		max_length=50
+		max_length=50,
 		)
 	
 	email = forms.EmailField(required=True)
